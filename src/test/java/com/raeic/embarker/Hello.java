a2c36574be276014790aa39ed4cc6afacb9305b4
Benjamin Tucker <betucker@ucsd.edu>
2020-04-08 23:17:01 -0700

    A test!

---
+++ src/test/java/com/raeic/embarker/Hello.java
@@ -0,0 +1,13 @@
+package com.raeic.embarker;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Hello {
+
+    @Test
+    void testsRun() {
+        assertEquals(2, 2);
+    }
+}
